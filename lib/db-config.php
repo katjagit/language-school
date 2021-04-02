ec55884561be6715e680e3ed999ca4b9133b6182
Ekaterina Starikova <katia.starikova@gmail.com>
2021-04-02 15:40:49 +0200

    Add files via upload

---
+++ lib/db-config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'db' => [
+        'host' => 'localhost',
+        'dbname' => 'school',
+        'user' => 'root',
+        'password' => 'root',
+        'charset' => 'utf8'
+    ]
+];
---
+++ lib/request.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+function request_method() : string
+{
+    return $_SERVER['REQUEST_METHOD'];
+}
+
+
+function request_is(string $method) : bool
+{
+    return strtolower(request_method()) === strtolower($method);
+}
+
+// Default Parameters
+
+function request($key = null)
+{
+    return array_extract($_POST, $key);
+}
+
+
+function query($key = null)
+{
+    return array_extract($_GET, $key);
+}
+
+
+function array_extract(array $array, $key = null)
+{
+    if ($key === null) {
+        return $array;
+    }
+    
+    if (is_array($key)) {
+        return array_intersect_key($array, array_flip($key));
+    }
+
+    return $array[$key] ?? '';
+}
