1eecbac5f3a4d0971a6c2e6969b69080f296a1d6
Ekaterina Starikova <katia.starikova@gmail.com>
2021-04-19 22:33:13 +0200

    Update bootstrap.php

--- parts/bootstrap.php
+++ parts/bootstrap.php
@@ -3,18 +3,17 @@ const PATH = '/Applications/MAMP/htdocs/language-school/';
 const BASE_URL = 'http://localhost:8888/language-school/';
 
 require_once PATH.'lib/authentication.php';
-//require_once PATH.'lib/database.php';
+require_once PATH.'lib/database.php';
 require_once PATH.'lib/request.php';
 require_once PATH.'lib/response.php';
 require_once PATH.'lib/session.php';
 require_once PATH.'lib/view.php';
-require_once PATH.'lib/classes/Database.class.php';
+require_once PATH.'lib/classes/Db.class.php';
 
-$db_config = require_once PATH.'lib/db-config.php';
-
-$db = new Database($db_config['db']);
+$db = new Db();
 
 session_start();
 
 $errors = [];
+
 
